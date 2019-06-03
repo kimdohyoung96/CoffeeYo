fca5a6cdf807e1c6f370cc0d263ff2e8675d609d
das109 <das97@naver.com>
2019-06-04 03:04:37 +0900

    user activity+name search

--- app/src/main/java/mobileApp/project/CoffeeYo/ReserveFragment.java
+++ app/src/main/java/mobileApp/project/CoffeeYo/ReserveFragment.java
@@ -3,68 +3,96 @@ package mobileApp.project.CoffeeYo;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ReserveFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ReserveFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class ReserveFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ReserveFragment.OnFragmentInteractionListener mListener;
+    private DatabaseReference mPostReference;
 
-    private OnFragmentInteractionListener mListener;
+    String uid;
+    ListView listView;
+    ArrayList<String> data;
+    ArrayAdapter<String> arrayAdapter;
+    int flag;
+    long cafeID;
 
     public ReserveFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReserveFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReserveFragment newInstance(String param1, String param2) {
-        ReserveFragment fragment = new ReserveFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reserve, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_reserve, container, false);
+        final Context contextRegister = container.getContext();
+        uid = getArguments().getString("uid");
+
+        data = new ArrayList<String>();
+        listView = (ListView)view.findViewById(R.id.orderlist);
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+        getFirebaseDatabase();
+
+
+        return view;
+    }
+
+    public void getFirebaseDatabase() {
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("onDataChange", "Data is Updated");
+                data.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String key = postSnapshot.getKey();
+                    Orderfirebase get = postSnapshot.getValue(Orderfirebase.class);
+                    String[] info = {get.cafe_id, get.cafe_name, get.order, get.state};
+                    String result = info[1] + "[" + info[2] + ", " + info[3] + "]";
+                    data.add(result);
+                    Log.d("getFirebaseDatabase", "key: " + key);
+                    Log.d("getFirebaseDatabase", "info: " + info[0] + info[1] + info[2] + info[3]);
+
+                }
+                arrayAdapter.clear();
+                arrayAdapter.addAll(data);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        };
+        mPostReference.child("user_list").child(uid).child("order").addValueEventListener(postListener);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,8 +105,8 @@ public class ReserveFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof ReserveFragment.OnFragmentInteractionListener) {
+            mListener = (ReserveFragment.OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -91,16 +119,6 @@ public class ReserveFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
